0db3e3f04971dfb046543ddef49d76f331e38ed1
Nigel <nigel.oketch@student.moringaschool.com>
2023-09-04 10:47:10 +0300

    complete

---
+++ main.py
@@ -0,0 +1,8 @@
+from customer import Customer
+from restarunt import Review
+
+name = Customer("Paul", "Gudoi")
+print(name.given_name)
+
+review1 = Review ("nigel","ohio","five star")
+print(review1)
