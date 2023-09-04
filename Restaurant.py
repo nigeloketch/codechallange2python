8f75c2d9e933fcca53f9805fffccb2bdb033ea18
Nigel <nigel.oketch@student.moringaschool.com>
2023-09-04 21:34:24 +0300

    completed

--- Restaurant.py
+++ Restaurant.py
@@ -1,8 +1,9 @@
-# restaurant.py
-
+# Define a Python class named Restaurant.
 class Restaurant:
+    # The constructor method (__init__) initializes the object with a 'name' attribute.
     def __init__(self, name):
-        self.name = name
+        self.name = name  # Store the 'name' attribute when creating a new instance.
 
+    # A method to get the name of the restaurant.
     def get_name(self):
-        return self.name
+        return self.name  # Return the 'name' attribute of the instance.
